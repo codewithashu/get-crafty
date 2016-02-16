7e02be1594328af9109cbcff86e9d89736bb9ab6
Jue Yang <jueyang22@gmail.com>
2016-02-16 14:56:59 -0500

    add explanation

--- illustrate/illustrate.js
+++ illustrate/illustrate.js
@@ -5,17 +5,20 @@ function makeArray(collection) {
 }
 
 function showTag(el) {
+
   var div = document.createElement('div'),
-    text = document.createTextNode(el.tagName);
+      text = document.createTextNode(el.tagName);
+
   div.className = 'tag';
   div.appendChild(text);
 
   return div
+
 }
 
 function showSection() {
 
-  makeArray(sections).forEach(function(section, i){
+  makeArray(sections).forEach(function(section){
     section.className = 'section-highlight';
   });
 
@@ -23,28 +26,36 @@ function showSection() {
 
 function showChildren() {
 
-  makeArray(sections).forEach(function(section,i){
+  makeArray(sections).forEach(function(section){
+
     var children = section.children;
-    makeArray(children).forEach(function(child,i){
+
+    makeArray(children).forEach(function(child){
       child.appendChild(showTag(child));
       child.className = 'children-highlight'
     })
+
   });
 
 }
 
 function clearText() {
-  var node,
-    walk = document.createTreeWalker(
-      document.body,
-      NodeFilter.SHOW_ELEMENT,
-      null,
-      false
-    );
 
+  var node,
+      walk = document.createTreeWalker(
+        document.body,
+        NodeFilter.SHOW_ELEMENT,
+        null,
+        false
+      );
+
+  // while is useful when the length of a collection is unknown
+  // in this case, the last walk.nextNode would be null (the end of document)
+  // that is why the looping stops
   while (node = walk.nextNode()) {
     if (node.tagName === 'H3' || node.tagName === 'P' ){
       node.innerText = '';
     }
   }
+
 }
